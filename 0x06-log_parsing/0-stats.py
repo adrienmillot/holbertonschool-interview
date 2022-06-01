5d55799631bac902ba65ba35861f95873c320633
Adrien <adrien.millot@gmail.com>
2022-06-01 15:02:48 +0200

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,47 +1,64 @@
 #!/usr/bin/python3
 """
-Module that parses a log and prints stats to stdout
+    Module that parses a log and prints stats to stdout
 """
 from sys import stdin
 
+count = 0
+global_size = 0
 status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
+    "200": 0, "301": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0,
     "500": 0
 }
 
-size = 0
+
+def fetch_data(line):
+    """
+        Parses the line and returns the status code and file size
+    """
+
+    matches = line.split()
+
+    return matches[-2], matches[-1]
 
 
-def print_stats():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
+def display_data():
+    """
+        Prints the stats to stdout
+    """
+
+    print("File size: {}".format(global_size))
+
     for status in sorted(status_codes.keys()):
         if status_codes[status]:
             print("{}: {}".format(status, status_codes[status]))
 
 
+def run():
+    """
+        Main function
+    """
+
+    try:
+        status_code, file_size = fetch_data(line)
+        global_size += int(file_size)
+
+        if (status_code in status_codes):
+            status_codes[status_code] += 1
+    except Exception:
+        pass
+    if count == 9:
+        display_data()
+        count = -1
+    count += 1
+
+
 if __name__ == "__main__":
-    count = 0
     try:
         for line in stdin:
-            try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
-            except:
-                pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
+            run(line)
     except KeyboardInterrupt:
-        print_stats()
+        display_data()
         raise
-    print_stats()
+    display_data()
